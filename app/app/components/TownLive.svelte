02f0756dd29b7a58dc9d037f46ffd68573e2b245
Clément Le Biez <clement.lebiez@yousign.com>
2020-08-06 09:40:40 +0200

    wip

---
+++ app/app/components/TownLive.svelte
@@ -0,0 +1,46 @@
+<script>
+  import {onMount} from "svelte";
+  import {getTownCurrentWeather} from "../utils/openWeatherMap.js";
+  // import TempHumidity from "./common/TempHumidity.svelte";
+  let temperature;
+  let humidity;
+  let currentWeather;
+
+  onMount(() => {
+    getTownCurrentWeather().then((res) => {
+      alert("COUCOU");
+      temperature = res.data.main.temp;
+      humidity = res.data.main.humidity;
+      currentWeather = res.data.weather[0];
+    });
+  });
+</script>
+
+<style>
+  .currentWeather {
+    margin-left: 12px;
+    margin-bottom: 12px;
+    display: flex;
+    /* justify-content: center; */
+    align-items: center;
+    text-transform: capitalize;
+    font-family: 'Playfair Display';
+  }
+
+  .currentWeather img {
+    width: 50px;
+  }
+</style>
+
+<flexboxLayout>
+<label text="coucou2" />
+  {#if currentWeather}
+    <flexboxLayout class="currentWeather">
+      <image
+        src={`http://openweathermap.org/img/wn/${currentWeather.icon}@2x.png`}
+        alt="Icon Weather" />
+      <label text={currentWeather.description} />
+    </flexboxLayout>
+  {/if}
+  <!-- <TempHumidity {temperature} {humidity} /> -->
+</flexboxLayout>
--- app/src/utils/frankyHome.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: process.env.FRANKYHOME_API_URL,
-});
-export const getLive = () => {
-  return instance
-    .get(`/live`);
-};
