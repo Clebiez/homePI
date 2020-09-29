4c526eb98bcf2b19ff014b48c8e1ecd98bedd654
Clément Le Biez <clement.lebiez@yousign.com>
2020-09-29 19:20:06 +0200

    ui

--- app/src/Live.js
+++ app/src/Live.js
@@ -1,45 +1,57 @@
-import React, {useEffect, useState} from "react";
-import {Layout, Text, Avatar, Card} from "@ui-kitten/components";
+import React, {useEffect, useState, useCallback} from "react";
+import {StyleSheet} from "react-native";
+import {Layout, Text, Avatar, Card, Button} from "@ui-kitten/components";
 
 import {getTownCurrentWeather} from "./utils/openWeatherUtils";
 
 import {getLive} from "./utils/live";
 
-const Header = ({children}) => (
-  <Text style={{textAlign: "center", padding: "8px"}} category="h4">
+const Header = ({children, retryCallback}) => (
+  <Text style={styles.headerTitle} category="h4">
     {children}
+    <Button style={styles.headerButton} onPress={retryCallback}>
+      Retry
+    </Button>
   </Text>
 );
-
-const HeaderHome = () => <Header>Home</Header>;
-const HeaderOutside = () => <Header>Outside</Header>;
 
 const Live = () => {
   const [outside, setOutside] = useState({});
   const [home, setHome] = useState({});
 
+  const getOutside = useCallback(async () => {
+    const {data} = await getTownCurrentWeather();
+    setOutside(data);
+  }, [setOutside]);
+
+  const getHome = useCallback(async () => {
+    const {data} = await getLive();
+    setHome(data);
+  }, [setHome]);
+
+  const HeaderHome = () => (
+    <Header style={styles.headerCard} retryCallback={getHome}>
+      Home
+    </Header>
+  );
+  const HeaderOutside = () => (
+    <Header retryCallback={getOutside}>Outside</Header>
+  );
+
   useEffect(() => {
-    async function getOutside() {
-      const {data} = await getTownCurrentWeather();
-      setOutside(data);
-    }
-
-    async function getHome() {
-      const {data} = await getLive();
-      setHome(data);
-    }
-
     getOutside();
     getHome();
-  }, []);
+  }, [getOutside, getHome]);
 
   return (
-    <Layout style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-      <Card header={HeaderHome}>
+    <Layout
+      style={{flex: 1, justifyContent: "space-evenly", alignItems: "center"}}
+    >
+      <Card style={styles.card} header={HeaderHome}>
         {home.temp && <Text category="h5">{home.temp}°C</Text>}
         {home.humidity && <Text category="h5">{home.humidity}%</Text>}
       </Card>
-      <Card header={HeaderOutside}>
+      <Card style={styles.card} header={HeaderOutside}>
         {outside.weather && (
           <>
             <Avatar
@@ -54,5 +66,22 @@ const Live = () => {
     </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    width: "90%",
+  },
+  headerCard: {
+    padding: "12px",
+  },
+  headerTitle: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  headerButton: {
+    marginLeft: '12px'
+  }
+});
 
 export default Live;
